cfebd7c65573208a69502593da1bfd9f1e003c45
Sri Harsha Sankarasetty <slssh.official@gmail.com>
2018-07-02 11:56:27 +0530

    Initial Beta Commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # botViaWeb
+
+Simple Bot using DNN (Tensorflow) and served using Django
+
+### Installing Requirements
+> Run the command in root directory (where manage.py lives)
+
+`pip install -r requirements.pip --no-index --find-links file:///tmp/packages`
+
+##Training
+Expected responses to speech requests can be configured in **Intents.json** file present in the same root folder
+##### Example
+```json
+{
+  "intents": [{
+      "tag": "greeting",
+      "patterns": ["Hi", "How are you", "Is anyone there?", "Hello", "Good day"],
+      "responses": ["Hello, thanks for visiting", "Good to see you again", "Hi there, how can I help?"],
+      "context_set": ""
+    },
+    {
+      "tag": "goodbye",
+      "patterns": ["Bye", "See you later", "Goodbye"],
+      "responses": ["See you later, thanks for visiting", "Have a nice day", "Bye! Come back again soon."]
+    }
+  ]
+}
+```
+## Firing the Server and Requests
+Start the server on desired port in localhost/any ip and port. Localhost and 8000 are the defaults
+```bash
+python manage.py runserver
+python manage.py runserver 10.1.1.1:2130
+```
+The server only accepts GET request (can be configured otherwise also) with **Speech** as query text
+
+`http://localhost:8000/?speech="Hi there, I am a sample text"`
+
+The Expected response will be
+```json
+{
+	result: {
+		"tag":"response"
+	}
+}
+```
---
+++ core/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, render_to_response
+import pickle
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+stemmer = LancasterStemmer()
+
+import json
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+model.load('./model.tflearn')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+context = {}
+
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    results = model.predict([bow(sentence, words)])[0]
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    if results:
+        while results:
+            for i in intents['intents']:
+                if i['tag'] == results[0][0]:
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        return random.choice(i['responses']),i['tag']
+
+            results.pop(0)
+@csrf_exempt
+def giveresponse(request):
+    if request.method == "GET":
+        try:
+            prediction, tag = response(request.GET["speech"])
+            dictionary = {tag:prediction}
+            responseDict = {"result": dictionary}
+            if tag == "hunger":
+                links = ["https://www.zomato.com/chennai","Feedme.com"]
+                responseDict.update({"links":links})
+            return JsonResponse(responseDict)
+        except Exception as e:
+            print(e)
+            return JsonResponse({"result":"Bad request"})
+    else:
+        return render_to_response("<h3>Only GET accepted!!</h3>")
